6059ecf62938b810286936dc76fe7e94231354d5
Asif Shakeel <asif.shakeel@gmail.com>
2025-12-13 08:45:03 -0800

    Add Tailwind CSS setup

--- src/analyses.js
+++ src/analyses.js
@@ -1,6 +1,33 @@
 export const ANALYSES = {
   basic_clinic: {
     label: "Basic Clinic Analysis",
-    files: ["patients", "visits", "metrics"],
+    files: {
+      patients: {
+        required_columns: [
+          "patientid",
+          "zip",
+          "dob",
+          "city",
+          "state",
+        ],
+      },
+      visits: {
+        required_columns: [
+          "patientid",
+          "visitdate",
+          "service",
+          "servicecharge",
+          "serviceduration",
+        ],
+      },
+      metrics: {
+        required_columns: [
+          "patientid",
+          "metricdate",
+          "painscore",
+          "mobilityscore",
+        ],
+      },
+    },
   },
 };
